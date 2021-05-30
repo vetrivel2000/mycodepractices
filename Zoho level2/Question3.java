85de29f441557b5afdc18a98e6dd2f348047a4f9
Vetrivel M <49315512+vetrivel2000@users.noreply.github.com>
2021-05-30 11:20:55 +0530

    Update Question3.java

---
+++ Zoho level2/Question3.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class Question3 {   
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        System.out.println("Enter the matrix size");
+        n=sc.nextInt();
+        int[][] arr = new int[n][n];
+        System.out.println("Enter the matrix");
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                arr[i][j]=sc.nextInt();
+            }
+        }
+        for(int i=n-1;i>=0;i--)
+        {
+            System.out.print(arr[0][i]+" ");
+        }
+        int i=1,j=1;
+        while(i<n-1 && j<n-1)
+        {
+            System.out.print(arr[i][j]+" ");
+            i++;
+            j++;
+        }
+        for(i=n-1;i>=0;i--)
+        {
+            System.out.print(arr[n-1][i]+" ");
+        }
+        sc.close();
+    } 
+}
