9f1b4224addc1734e6f3d61e3788dcbff91cfa01
Vetrivel M <49315512+vetrivel2000@users.noreply.github.com>
2021-07-02 13:19:23 +0530

    Update printEvenOdd.java

--- Task-1/printEvenOdd.java
+++ Task-1/printEvenOdd.java
@@ -12,15 +12,21 @@ package evenodd;
 public class printEvenOdd {
     public void oddEven(int array[],int size)
     {
+        int j=-1,temp;
         for(int i=0;i<size;i++)
         {
             if(array[i]%2==0)
-               System.out.print(array[i]+" ");
+            {
+                j++;
+                temp=array[i];
+                array[i]=array[j];
+                array[j]=temp;
+            }
         }
+        System.out.println("The array after seperating even numbers first and odd numbers next:");
         for(int i=0;i<size;i++)
         {
-            if(array[i]%2==1)
-               System.out.print(array[i]+" ");
+            System.out.print(array[i]+" ");
         }
     }
 }
