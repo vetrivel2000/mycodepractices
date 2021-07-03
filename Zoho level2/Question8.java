fcd5194b5b42a42613347caa0fc90bbbac0c74c5
Vetrivel M <49315512+vetrivel2000@users.noreply.github.com>
2021-07-03 20:44:51 +0530

    Update Question8.java

--- Zoho level2/Question8.java
+++ Zoho level2/Question8.java
@@ -10,27 +10,90 @@ I want to
 */
 
 import java.util.*;
-public class RemoveFragment {
+public class Question8{
     public static void main(String args[])
     {
         Scanner sc = new Scanner(System.in);
         String str1 = sc.nextLine();
         String str2 = sc.nextLine();
         String str3 = sc.nextLine();
-        
-       String words1[] = str1.split(" ");
-       String words2[] = str2.split(" ");
-       String words3[] = str3.split(" ");
-        for(int i=0;i<words1.length;i++)
-        {
-            for(int j=0;j<words2.length;j++)
+        char []s1 = str1.toCharArray();
+        char []s2 = str2.toCharArray();
+        char []s3 = str3.toCharArray();
+        int c1=0,c2=0,c3=0;
+        char words1[][]=new char[100][100];
+        char words2[][]=new char[100][100];
+        char words3[][]=new char[100][100];
+        int p=0,q=0,r=0;
+        String A[]=new String[100];
+        String B[]= new String[100];
+        String C[]=new String[100];
+        for(int i=0;i<s1.length;i++)
+        { 
+            if(s1[i]==' ')
             {
-                for(int k=0;k<words3.length;k++)
-                {
-                    if(words1[i].equals(words2[j])&&words2[j].equals(words3[k])&&words3[k].equals(words1[i]))
-                        System.out.print(words1[i]+" ");
-                }
+                p=0;
+                c1++;
+            }
+            if(s1[i]!=' ')
+            {
+                words1[c1][p] = s1[i];
+                p++;
             }
         }
-    }
+        c1++;
+        for(int i=0;i<s2.length;i++)
+        {
+            if(s2[i]==' ')
+            {
+                q=0;
+                c2++;
+            }
+            if(s2[i]!=' ')
+            {
+                words2[c2][q] = s2[i];
+                q++;
+            }
+        }
+        c2++;
+        for(int i=0;i<s3.length;i++)
+        {
+            if(s3[i]==' ')
+            {
+                r=0;
+                c3++;
+            }
+            if(s3[i]!=' ')
+            {
+                words3[c3][r] =s3[i];
+                r++;
+            }
+       }
+        c3++;
+        
+        
+        for(int i=0;i<c1;i++)
+        {
+                A[i]=String.valueOf(words1[i]);
+        }
+        for(int i=0;i<c2;i++)
+        {
+                B[i]=String.valueOf(words2[i]);
+        }
+        for(int i=0;i<c3;i++)
+        {
+                C[i]=String.valueOf(words3[i]);
+        }
+        for(int i=0;i<c1;i++)
+        {
+            for(int j=0;j<c2;j++)
+            {
+                for(int k=0;k<c3;k++)
+                {
+                    if(A[i].equals(B[j])&&B[j].equals(C[k])&&C[k].equals(A[i]))
+                        System.out.print(A[i]+" ");
+               }
+            }
+        }
+ }
 }
