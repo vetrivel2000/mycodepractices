cfe29bcf26aa2b0224f09538152304183847a7c8
Vetrivel M <49315512+vetrivel2000@users.noreply.github.com>
2021-07-03 11:39:52 +0530

    Update Question7.java

--- Zoho level2/Question7.java
+++ Zoho level2/Question7.java
@@ -6,44 +6,17 @@ find my reversal
 output:
 dnif ym lasrever
 */
+
 import java.util.*;
-public class Question7 
-{
-static void rev(char str[],int i,int j)
-{
-	char temp;
-	while(i<=j)
-	{
-		temp=str[i];
-		str[i]=str[j];
-		str[j]=temp;
-		i++;
-		j--;
-	}
-}
-static char[] reverse(char s[])
-{
-	int i=0;
-	for(int j=0;j<s.length;j++)
-	{
-		if(s[j]==' ')
-		{
-			rev(s,i,j);
-			i=j+1;
-		}
-	}
-	rev(s,i,s.length-1);
-	return s;
-}
-public static void main(String[] args)
-{
-	 Scanner sc = new Scanner(System.in);
-	 String str=sc.nextLine();
-	 char st[]=reverse(str.toCharArray());
-	 for(int i=0;i<st.length;i++)
-	 {
-		 System.out.print(st[i]);
-	 }
-	 sc.close();
-}
+public class Question7 {
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String[] temp = str.split(" ");
+        for(String temp1 : temp) {
+            StringBuilder st = new StringBuilder(temp1);
+            System.out.print(st.reverse()+" ");
+        }
+    }
 }
